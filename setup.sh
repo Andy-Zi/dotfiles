3412342b3d90f990ef88c58e046f39fc3da26875
Andy-Zi <andreas@zieglerit.org>
2024-01-19 07:37:59 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@
 ln -s ~/.config/dotfiles/nvim ~/.config/nvim
 ln -s ~/.config/dotfiles/.p10k.zsh ~/.p10k.zsh
 ln -s ~/.config/dotfiles/.oh-my-zsh ~/.oh-my-zsh
-ln -s ~/.config/dotfiles/.tmux ~/.tmux
 ln -s ~/.config/dotfiles/.zshrc ~/.zshrc
 ln -s ~/.config/dotfiles/.tmux.conf ~/.tmux.conf
 
@@ -12,8 +11,10 @@ ln -s ~/.config/dotfiles/.tmux.conf ~/.tmux.conf
 sudo apt-get install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Install zsh plugins
+# Install p10k theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 
+# Install zsh plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
