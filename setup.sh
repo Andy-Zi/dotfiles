1f5af17deca8f2072ab6868bf99a7eb23dcb935e
Andy-Zi <andreas@zieglerit.org>
2024-01-24 21:02:02 +0100

    updated tmux config

--- setup.sh
+++ setup.sh
@@ -29,4 +29,3 @@ rm -rf zsh-syntax-highlighting
 # Install tmux
 sudo apt-get install tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-git clone https://github.com/jimeh/tmuxifier.git ~/.tmuxifier
